1e728a9a4c0afb2cbe6a8c1d929efad3a273cd69
Philipp Mos. <46461566+philippmos@users.noreply.github.com>
2025-06-22 10:32:59 +0200

    Move Overview component to dashboard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Overview } from './components/overview/overview';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: Overview,
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,15 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navigation } from "./layout/navigation/navigation";
 import { PageHeader } from './layout/page-header/page-header';
-import { Overview } from './components/overview/overview';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     Navigation,
-    PageHeader,
-    Overview
+    PageHeader
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
