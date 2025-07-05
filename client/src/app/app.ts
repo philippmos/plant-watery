3760adfebc0a6002cb8e5d82e4c7fdee32310f30
Philipp Mos. <46461566+philippmos@users.noreply.github.com>
2025-07-05 22:46:42 +0200

    Add footer component: implement footer layout, styles, and current year display

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navigation } from "./components/layout/navigation/navigation";
 import { PageHeader } from "./components/layout/page-header/page-header";
+import { FooterComponent } from "./components/layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     Navigation,
-    PageHeader
+    PageHeader,
+    FooterComponent
 ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
